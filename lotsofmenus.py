42724e0b678d56fffdadaede8088df9ca62d20fa
vagrant <vagrant@vagrant.vm>
2019-03-25 10:43:16 +0000

    included dummy images, added image column to Restaurant table and image paths to restaurants data

--- lotsofmenus.py
+++ lotsofmenus.py
@@ -20,7 +20,7 @@ session = DBSession()
 
 
 # Menu for UrbanBurger
-restaurant1 = Restaurant(name="Urban Burger")
+restaurant1 = Restaurant(name="Urban Burger", image = "/images/restaurantImage_1")
 
 session.add(restaurant1)
 session.commit()
@@ -82,7 +82,7 @@ session.commit()
 
 
 # Menu for Super Stir Fry
-restaurant2 = Restaurant(name="Super Stir Fry")
+restaurant2 = Restaurant(name="Super Stir Fry", image = "/images/restaurantImage_2")
 
 session.add(restaurant2)
 session.commit()
@@ -126,7 +126,7 @@ session.commit()
 
 
 # Menu for Panda Garden
-restaurant1 = Restaurant(name="Panda Garden")
+restaurant1 = Restaurant(name="Panda Garden", image = "/images/restaurantImage_3")
 
 session.add(restaurant1)
 session.commit()
@@ -164,7 +164,7 @@ session.commit()
 
 
 # Menu for Thyme for that
-restaurant1 = Restaurant(name="Thyme for That Vegetarian Cuisine ")
+restaurant1 = Restaurant(name="Thyme for That Vegetarian Cuisine ", image = "/images/restaurantImage_4")
 
 session.add(restaurant1)
 session.commit()
@@ -208,7 +208,7 @@ session.commit()
 
 
 # Menu for Tony's Bistro
-restaurant1 = Restaurant(name="Tony\'s Bistro ")
+restaurant1 = Restaurant(name="Tony\'s Bistro ", image = "/images/restaurantImage_5")
 
 session.add(restaurant1)
 session.commit()
@@ -246,7 +246,7 @@ session.commit()
 
 
 # Menu for Andala's
-restaurant1 = Restaurant(name="Andala\'s")
+restaurant1 = Restaurant(name="Andala\'s", image = "/restaurantImage__6/")
 
 session.add(restaurant1)
 session.commit()
@@ -284,7 +284,7 @@ session.commit()
 
 
 # Menu for Auntie Ann's
-restaurant1 = Restaurant(name="Auntie Ann\'s Diner ")
+restaurant1 = Restaurant(name="Auntie Ann\'s Diner ", image = "/images/restaurantImage_7")
 
 session.add(restaurant1)
 session.commit()
@@ -334,7 +334,7 @@ session.commit()
 
 
 # Menu for Cocina Y Amor
-restaurant1 = Restaurant(name="Cocina Y Amor ")
+restaurant1 = Restaurant(name="Cocina Y Amor ", image = "/images/restaurantImage_8")
 
 session.add(restaurant1)
 session.commit()
@@ -353,7 +353,7 @@ session.add(menuItem2)
 session.commit()
 
 
-restaurant1 = Restaurant(name="State Bird Provisions")
+restaurant1 = Restaurant(name="State Bird Provisions", image = "/images/restaurantImage_9")
 session.add(restaurant1)
 session.commit()
 

bda6b584295bd7058a6a7161b9fb6ab943280e88
vagrant <vagrant@vagrant.vm>
2019-03-26 15:06:05 +0000

    added images to menuItems

--- finalProject.py
+++ finalProject.py
@@ -31,7 +31,7 @@ def showRestaurantMenu(restaurant_id):
 @app.route('/restaurant/new/', methods=['GET', 'POST'])
 def addNewRestaurant():
     if request.method == 'POST':
-        newRestaurant = Restaurant(name=request.form['name'])
+        newRestaurant = Restaurant(name=request.form['name'], image =request.form['image'])
         session.add(newRestaurant)
         session.commit()
         return redirect(url_for('showRestaurants'))
@@ -41,7 +41,7 @@ def addNewRestaurant():
 @app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
 def addNewMenuItem(restaurant_id):
     if request.method == 'POST':
-        newMenuItem = MenuItem(name=request.form['name'], description=request.form['description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
+        newMenuItem = MenuItem(name=request.form['name'], description=request.form['description'], price=request.form['price'], course=request.form['course'], dish_image= request.form['dish_image'], restaurant_id=restaurant_id)
         session.add(newMenuItem)
         session.commit()
         return redirect(url_for('showRestaurantMenu', restaurant_id = restaurant_id))
@@ -54,6 +54,8 @@ def editRestaurant(restaurant_id):
     if request.method == 'POST':
         if request.form['name']:
             restaurant.name = request.form['name']
+        if request.form['image']:
+            restaurant.image = request.form['image']
         session.add(restaurant)
         session.commit()
         return redirect(url_for('showRestaurants'))
@@ -82,6 +84,8 @@ def editMenuItem(restaurant_id, menuItem_id):
             menuItem.price = request.form['price']
         if request.form['course']:
             menuItem.course = request.form['course']
+        if request.form['dish_image']:
+            menuItem.dish_image = request.form['dish_image']
         session.add(menuItem)
         session.commit()
         return redirect(url_for('showRestaurantMenu', restaurant_id = restaurant_id))
@@ -96,7 +100,7 @@ def deleteMenuItem(restaurant_id, menuItem_id):
         session.commit()
         return redirect(url_for('showRestaurantMenu', restaurant_id = restaurant_id))
     else:
-        return render_template('deleteMenuItem.html', restaurant_id = restaurant_id, menuItem_id = menuItem_id,menuItem = menuItemToDelete)
+        return render_template('deleteMenuItem.html', restaurant_id = restaurant_id, menuItem_id = menuItem_id, menuItem = menuItemToDelete)
 
 @app.route('/restaurants/JSON')
 def restaurantsJSON():
@@ -115,18 +119,6 @@ def restaurantMenuJSON(restaurant_id):
 def menuItemJSON(restaurant_id, menuItem_id):
     menuItem = session.query(MenuItem).filter_by(id = menuItem_id, restaurant_id = restaurant_id).one()
     return jsonify(MenuItem = [menuItem.serialize])
-
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-
-
 
 if __name__ == '__main__':
     app.debug = True
